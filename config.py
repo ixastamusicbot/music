e16fdba98d5319226416dd13ec7e0dd8677ce045
⋏ 𝛅 𝛕 ⋏ <astakhan63@gmail.com>
2025-10-22 21:42:29 +0530

    Update config.py
    Signed-off-by: ⋏ 𝛅 𝛕 ⋏ <astakhan63@gmail.com>

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 # Vars For API End Pont.
-YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com')  # xBit Music Endpoint.
+YTPROXY_URL = getenv("YTPROXY_URL", 'https://github.com/ixastamusicbot/music')  # xBit Music Endpoint.
 YT_API_KEY = "NxGBNexGenBots4e1026"  # Fixed API key inserted
 
 ## Other vars
